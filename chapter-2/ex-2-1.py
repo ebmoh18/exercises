a0c149c96479427c43293962fa194befa014bb10
ebrahim Mohamad <ebrahim-mohamad@student.kirkwood.edu>
2017-09-19 13:17:43 +0000

    Address program# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# On branch master# Your branch is up-to-date with 'origin/master'.## Changes to be committed:#	modified:   ex-2-1.py## Untracked files:#	../ex-2-1.py#

--- chapter-2/ex-2-1.py
+++ chapter-2/ex-2-1.py
@@ -6,19 +6,19 @@
 # It will display the name and contact information for a teacher.
 
 # display the full name
-
+print("Ebrahim Mohamad")
 
 # display the address
-
+print("2535BartletRdAp2C")
 
 # display the city, state and zip
-
+print("Iowa city,IA,52246")
 
 # display the phone number
-
+print("3194408631")
 
 # display the course area
-
+print("Course area")
 
 
 
